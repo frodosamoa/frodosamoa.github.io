6dac6d05c08fbb30bdaa075cccb3cbeb211d97ba
Andrew Kowalczyk <andrewrkowalczyk@gmail.com>
2018-01-21 21:46:19 +0100

    remvoe files that I won't be using

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Interactive from 'react-interactive';
 import { Switch, Route } from 'react-router-dom';
 import Home from './Home';
 import ExampleComponent from './ExampleComponent';
@@ -10,36 +9,12 @@ import s from '../styles/app.style';
 export default function App() {
   return (
     <div style={s.root}>
-      <h1 style={s.title}>Single Page Apps for GitHub Pages</h1>
-      <Interactive
-        as="a"
-        href="https://github.com/rafrex/spa-github-pages"
-        style={s.repoLink}
-        {...s.link}
-      >https://github.com/rafrex/spa-github-pages</Interactive>
-
-      <nav style={s.breadcrumbs}>
-        <Breadcrumbs />
-      </nav>
+      <h1 style={s.title}>Andrew Kowalczyk</h1>
 
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/example" component={ExampleComponent} />
         <Route component={PageNotFound} />
       </Switch>
-
-      <div style={s.creditLine}>
-        <Interactive
-          as="a"
-          href="http://www.rafaelpedicini.com"
-          interactiveChild
-          focus={{}}
-          touchActive={{}}
-          touchActiveTapOnly
-        >
-          Code and concept by <span {...s.childLink}>Rafael Pedicini</span>
-        </Interactive>
-      </div>
     </div>
   );
 }
