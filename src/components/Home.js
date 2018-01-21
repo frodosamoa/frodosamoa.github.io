db60c56df26220498c695ba8a38c35d102493a99
Andrew Kowalczyk <andrewrkowalczyk@gmail.com>
2018-01-21 21:55:05 +0100

    clean up home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,11 @@
 import React from 'react';
-import { Code } from '../styles/style';
 import s from '../styles/home.style';
 
 export default function home() {
   return (
     <div>
       <p style={s.p}>
-        This is an example single page app built
-        with React and React&nbsp;Router using {' '}
-        <Code>BrowserRouter</Code>. Navigate with the links below and
-        refresh the page or copy/paste the url to test out the redirect
-        functionality deployed to overcome GitHub&nbsp;Pages incompatibility
-        with single page apps (like this one).
-      </p>
-      <p style={s.p}>
-        Please see the repo readme for instructions on how to
-        use this boilerplate to deploy your own single page app using GitHub Pages.
+        hey there! my name is Andrew Kowalczyk and I am a human.
       </p>
     </div>
   );
