09fae417030a8074118c97981246e615e5748b21
Andrew Kowalczyk <andrewrkowalczyk@gmail.com>
2018-01-30 13:13:14 +0100

    add tufte-css to project and remove JS styles

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,11 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './Home';
 import PageNotFound from './PageNotFound';
-import s from '../styles/app.style';
 
 export default function App() {
   return (
-    <div style={s.root}>
-      <h1 style={s.title}>Andrew Kowalczyk</h1>
+    <div>
+      <h1>Andrew Kowalczyk</h1>
 
       <Switch>
         <Route exact path="/" component={Home} />
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import s from '../styles/home.style';
 
 export default function home() {
   return (
     <div>
-      <p style={s.p}>
-        hey there! my name is Andrew Kowalczyk and I am a human.
+      <p>
+        hey there! my name is Andrew Kowalczyk and I am a human
+      </p>
+      <p>
+        find me on instagram, goodreads, medium, github
       </p>
     </div>
   );
